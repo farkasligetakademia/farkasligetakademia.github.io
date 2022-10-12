e38dfc065119e5bd4290bd6cb6a231b6ce4f7d49
farkasligetakademia <113542972+farkasligetakademia@users.noreply.github.com>
2022-10-12 20:34:38 +0200

    Update fla.js

--- fla.js
+++ fla.js
@@ -1,9 +1,3 @@
-// vh
-
-/*let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);*/
-
 // Open & Close Navigation
 
 var OPEN = false;
